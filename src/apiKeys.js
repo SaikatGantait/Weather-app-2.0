2fa00e6c7e87a1188a5667ad458154200e45f486
SAIKAT <88987186+SaikatGantait@users.noreply.github.com>
2023-09-06 20:20:44 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,4 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
-  base: "https://api.openweathermap.org/data/2.5/",
-};
+  key: "0e486bc00355ca3b65a5ddbc3cb55099",
+  base: "https://api.openweathermap.org/data/2.5/",};
