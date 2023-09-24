06e8d5b5cb0c828b7a71539ccd82bf661ab73390
SAIKAT <88987186+SaikatGantait@users.noreply.github.com>
2023-09-24 18:21:56 +0530

    Update apiKeys.js

--- Code/apiKeys.js
+++ Code/apiKeys.js
@@ -1,5 +1,4 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "{Your API Key Here}",
+  key: "0e486bc00355ca3b65a5ddbc3cb55099",
   base: "https://api.openweathermap.org/data/2.5/",
 };
