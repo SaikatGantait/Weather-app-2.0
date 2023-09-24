9fb90cba9371da22f930a5fd320a7d6055ae435f
SAIKAT <88987186+SaikatGantait@users.noreply.github.com>
2023-09-24 05:35:43 +0530

    Delete src directory

--- src/apiKeys.js
+++
@@ -1,4 +0,0 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
-module.exports = {
-  key: "0e486bc00355ca3b65a5ddbc3cb55099",
-  base: "https://api.openweathermap.org/data/2.5/",};
